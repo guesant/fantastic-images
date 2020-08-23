47db14ff252629996cdd6bea57b79b724b0d675c
Gabriel Rodrigues <jipacoding@gmail.com>
2020-08-23 19:01:03 -0400

    feat(lib): canvas/removeAllObjects

---
+++ packages/lib/src/canvas/remove-all-objects/index.ts
@@ -0,0 +1,7 @@
+import { fabric as FabricTypes } from 'fabric';
+
+const removeAllObjects = ({ canvas }: { canvas: FabricTypes.Canvas }) => {
+  canvas.getObjects().forEach(obj => canvas.remove(obj));
+};
+
+export default removeAllObjects;
