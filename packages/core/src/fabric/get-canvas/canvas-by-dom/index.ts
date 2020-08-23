a959c6575782e15ee1df06a6ea39172e3ca326c1
Gabriel Rodrigues <jipacoding@gmail.com>
2020-08-23 19:26:22 -0400

    feat(core): fabric/getCanvas/canvasByDom

---
+++ packages/core/src/fabric/get-canvas/canvas-by-dom/index.ts
@@ -0,0 +1,20 @@
+import { fabric as FabricTypes } from 'fabric';
+import { Template } from '@fantastic-images/types';
+
+const canvasByDom = ({ fabric }: { fabric: any }) => ({
+  document,
+}: {
+  document: Document;
+}) => ({ wrapper, id }: { wrapper: HTMLElement; id: string }) => (
+  template: Template
+): FabricTypes.Canvas => {
+  Array.from(wrapper.children || []).forEach(child => child.remove());
+  const domcanvas = document.createElement('canvas');
+  domcanvas.id = id;
+  domcanvas.width = Math.max(template.model.sketch.width, 0);
+  domcanvas.height = Math.max(template.model.sketch.height, 0);
+  wrapper.append(domcanvas);
+  return new fabric.Canvas(id);
+};
+
+export default canvasByDom;
---
+++ packages/core/src/fabric/get-canvas/index.ts
@@ -0,0 +1,1 @@
+export { default as canvasByDOM } from './canvas-by-dom';
