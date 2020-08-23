9f1ff08c9ba72625843852343c642d2253b256d8
Gabriel Rodrigues <jipacoding@gmail.com>
2020-08-23 18:55:58 -0400

    feat(lib): buildObject

---
+++ packages/lib/src/build-object/index.ts
@@ -0,0 +1,20 @@
+import { fabric as FabricTypes } from 'fabric';
+import { SupportedObjects } from '@fantastic-images/types';
+
+const buildObject = ({ fabric }: { fabric: any }) => ({
+  object,
+}: {
+  object: FabricTypes.Object;
+}): Promise<SupportedObjects> =>
+  new Promise(resolve => {
+    switch (object.type) {
+      case 'image':
+        return fabric.Image.fromObject(object, resolve);
+      case 'textbox':
+        return fabric.Textbox.fromObject(object, resolve);
+      default:
+        return new fabric.Object() as FabricTypes.Object;
+    }
+  });
+
+export default buildObject;
