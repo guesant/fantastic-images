210b548091567129d3de0ef04b2a2afac4c11244
Gabriel Rodrigues <jipacoding@gmail.com>
2020-08-23 21:02:20 -0400

    feat(lib): model/mutations/UPDATE_STATIC_IMAGE

---
+++ packages/lib/src/model/mutations/update-static-image/index.test.ts
@@ -0,0 +1,18 @@
+import UPDATE_STATIC_IMAGE from '../update-static-image';
+
+test('Update static image of template based on idx', () => {
+  expect(
+    UPDATE_STATIC_IMAGE(
+      3,
+      '###' as any
+    )({
+      model: {
+        staticImages: ['a', 'b', 'c', 'd'],
+      } as any,
+    })
+  ).toEqual({
+    model: {
+      staticImages: ['a', 'b', 'c', '###'],
+    },
+  });
+});
---
+++ packages/lib/src/model/mutations/update-static-image/index.ts
@@ -0,0 +1,19 @@
+import { Template, TemplateStaticImage } from '@fantastic-images/types';
+
+const UPDATE_STATIC_IMAGE = (idx: number, updated: TemplateStaticImage) => (
+  template: Template
+): Template => {
+  const { model } = template;
+  const { staticImages } = model;
+  const before = staticImages.slice(0, idx);
+  const after = staticImages.slice(idx + 1);
+  return {
+    ...template,
+    model: {
+      ...template.model,
+      staticImages: [...before, updated, ...after],
+    },
+  };
+};
+
+export default UPDATE_STATIC_IMAGE;
