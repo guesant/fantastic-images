e39f70f32ad5846867b638041f5854516e6e5bb6
Gabriel Rodrigues <jipacoding@gmail.com>
2020-08-23 20:58:48 -0400

    feat(lib): model/mutations/ADD_STATIC_IMAGE

---
+++ packages/lib/src/model/mutations/add-static-image/index.test.ts
@@ -0,0 +1,18 @@
+import ADD_STATIC_IMAGE from '../add-static-image';
+
+test('Should add static image to template', () => {
+  expect(
+    ADD_STATIC_IMAGE({ staticImages: [{ url: '#', position: 'back' }] })({
+      model: {
+        staticImages: [{ url: '@', position: '@' }],
+      } as any,
+    })
+  ).toEqual({
+    model: {
+      staticImages: [
+        { url: '@', position: '@' },
+        { url: '#', position: 'back' },
+      ],
+    },
+  });
+});
--- packages/lib/src/model/mutations/index.ts
+++ packages/lib/src/model/mutations/index.ts
@@ -1,1 +1,2 @@
 export { default as ADD_OBJECT } from './add-object';
+export { default as ADD_STATIC_IMAGE } from './add-static-image';
